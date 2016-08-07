86680f2fac60aa84669c0e0c897307e27c72c9b1
Brad Searle <bobthebutcher@users.noreply.github.com>
2016-08-07 12:08:22 +1000

    created PyEZ class and added init and get_facts methods

---
+++ junos.py
@@ -0,0 +1,25 @@
+from jnpr.junos import Device
+from jnpr.junos.exception import ConnectAuthError, ConnectTimeoutError
+
+
+class PyEZ(object):
+    def __init__(self, host, username, password, timeout=5):
+        self.host = host
+        self.username = username
+        self.password = password
+        self.timeout = timeout
+
+        self.conn = Device(host=self.host, user=self.username, passwd=self.password,
+                           timeout=self.timeout)
+
+        try:
+            self.conn.open()
+
+        except ConnectAuthError as e:
+            raise e
+
+        except ConnectTimeoutError as e:
+            raise e
+
+    def get_facts(self):
+        return self.conn.facts
